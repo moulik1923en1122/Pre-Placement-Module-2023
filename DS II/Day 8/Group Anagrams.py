95a7d1d59fa8aeae07c4174a893152ed6cb819a2
Moulik <moulik.1923en1122@kiet.edu>
2022-08-22 15:56:45 +0530

    Create Group Anagrams.py

---
+++ DS II/Day 8/Group Anagrams.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sort_word(self, word):
+        return ''.join(sorted(word))
+
+    def update_anagrams_dict(self, anagrams_dict, sorted_word, word):
+        if sorted_word in anagrams_dict:
+            value_arr = anagrams_dict[sorted_word]
+            value_arr.append(word)
+            anagrams_dict[sorted_word] = value_arr
+        else:
+            anagrams_dict[sorted_word] = [word]
+
+        return anagrams_dict
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_dict = {}
+        for word in strs:
+            sorted_word = self.sort_word(word)
+            anagrams_dict = self.update_anagrams_dict(anagrams_dict,
+                                                      sorted_word,
+                                                      word)
+
+        result = []
+        for key in anagrams_dict:
+            result.append(anagrams_dict[key])
+
+        return result
